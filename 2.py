037d16df57a3a97910f6200234af3628bfe8c8f7
Kira <97297341+KiraKova@users.noreply.github.com>
2022-04-04 16:52:56 +0700

    Домашняя работа по 6 уроку

--- 2.py
+++ 2.py
@@ -1,11 +1,16 @@
-def my_f(name, lastname, year, city, email, phone):
-    return f"Имя: {name}," \
-           f" Фамилия: {lastname}, " \
-           f"Год рождения: {year}, " \
-           f"Город: {city}, " \
-           f"Email: {email}," \
-           f"Телефон: {phone}"
+class Road:
+
+    def __init__(self, _length, _width, v, b):
+        self._lenght = _length
+        self._width = _width
+        self.v = v
+        self.b = b
+
+    def mass(self):
+        return (self._lenght * self._width * self.v * self.b) / 1000
 
 
-print(my_f(name=input('Введите имя: '), lastname=input('Введите фамилию: '), year=input('Год рождения: '),
-           city=input('Город проживания: '), email=input('Email: '), phone=input('Телефон: ')))
+r = Road(20, 5000, 25, 5)
+
+print(f'Результат: {int(r.mass())} т.')
+
